6f0cdfedc09ef93ab3c8c7c3c185be6415d6f1d6
MaxShishkov <nouxau_1@yahoo.com>
2016-09-19 23:39:00 -0500

    add BankStructs
    learning structs in C#

---
+++ BankStructs/BankStructs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace BankStructs
+{
+
+    enum AccountState
+    {
+        New,
+        Active,
+        UnderAudit,
+        Frozen,
+        Closed
+    };
+
+    struct Account
+    {
+        public AccountState State;
+        public string Name;
+        public string Address;
+        public int AccountNumber;
+        public int Balance;
+        public int Overdragt;
+    }
+
+    class Program
+    {
+
+        //public static void PrintAccount(Account a);
+
+        static void Main(string[] args)
+        {
+            const int MAX_CUST = 100;
+            Account[] Bank = new Account[MAX_CUST];
+            Bank[0].Name = "Rob";
+            Bank[0].Address = "Rob's Address";
+            Bank[0].Balance = 100000;
+            Bank[0].AccountNumber = 123456789;
+            Bank[0].State = AccountState.Active;
+
+            PrintAccount(Bank[0]);
+
+            Console.ReadLine();
+
+        }
+
+        public static void PrintAccount(Account a)
+        {
+            Console.WriteLine("Name: " + a.Name);
+            Console.WriteLine("Address: " + a.Address);
+            Console.WriteLine("Balance: " + a.Balance);
+        }
+    }
+}
